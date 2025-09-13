5b41f581e6c80a60e5d12c3bd0cd3f77fb104a52
proyectocolque <colquecondoricarla28@gmail.com>
2025-09-12 23:41:43 -0400

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,60 @@
+<template>
+
+  <div id="app">
+    <img alt="Logo" src="https://tse1.mm.bing.net/th/id/OIP.GZ84FosySqKw3nQa5oLLPQHaHa?r=0&rs=1&pid=ImgDetMain&o=7&rm=3">
+      <div>
+    <h1> MI APLICACION DE UN ALMACEN CON VUE.JS</h1>
+    <h3>Diseñado los componentes por Carla Colque Condori</h3>
+  </div>
+  <PresentacionCard
+  titulo="Almcacen de Multi Abarrotes"
+  />
+    <PresentacionCard
+      titulo="Bienvenidos al almacen Casa Del Rio"
+      linea1="Ofrecemos todo por mayor "
+      marca="jiji"
+      linea2="Ven y aprovecha la compra de todo tipo de productos"
+    />
+    <PresentacionCard
+      titulo="Asi que animate"
+      linea1="Aprovecha que es con aumento cada producto que vas llevar"
+      marca="jsjsj"
+      linea2="Recuerda que te atenderemos con mucho gusto"
+    />
+  
+  </div>
+</template>
+
+<script>
+import PresentacionCard from './components/proyecjt.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PresentacionCard
+  }
+}
+</script>
+
+
+<style>
+
+#app {
+
+  background-color: #1a1a1a;
+
+
+  background-image: url('https://tse3.mm.bing.net/th/id/OIP._M6NVD8nVqYcZtaD99lzlAHaE7?r=0&rs=1&pid=ImgDetMain&o=7&rm=');
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+
+  min-height: 100vh;
+
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #01010f;
+  margin: 0; 
+  padding: 0;
+}
+</style>
---
+++ proyecjt.vue
@@ -0,0 +1,63 @@
+<!-- src/components/Presentacion.vue -->
+<template>
+  <div class="presentacion-card">
+    <h1>{{ titulo }}</h1>
+    <p>{{ linea1 }} <span class="resaltado">{{ marca }}</span></p>
+    <p>{{ linea2 }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PresentacionCard',
+  props: {
+    titulo: {
+      type: String,
+      default: 'Título por defecto'
+    },
+    linea1: {
+      type: String,
+      default: ''
+    },
+    linea2: {
+      type: String,
+      default: ''
+    },
+    marca: {
+      type: String,
+      default: 'Vue.js'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.presentacion-card {
+  background: rgba(117, 196, 196, 0.429);
+  padding: 30px;
+  border-radius: 12px;
+  text-align: center;
+  max-width: 600px;
+  margin: 20px auto;
+  box-shadow: 0 10px 30px rgba(0,0,0,0.2);
+}
+
+.presentacion-card h1 {
+  color: rgb(56, 4, 34);
+  margin-bottom: 20px;
+  font-size: 2.5rem;
+}
+
+.presentacion-card p {
+  font-size: 1rem;
+  color: rgb(1, 32, 5);
+  margin: 10px 0;
+}
+
+.resaltado {
+  background: palevioletred;
+  font-size: 1.2rem;
+  padding: 2px 4px;
+  border-radius: 4px;
+}
+</style>
